b6873949bfc646f4d4e635d8e99fc713fbcbd28b
leunglicreative <leunglicreative@gmail.com>
2026-03-10 15:38:34 +0700

    update v.1.6

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Ganti dengan URL dan ANON KEY dari dashboard Supabase Anda
-const supabaseUrl = 'https://wweflunodocsdhbpnqxv.supabase.co'
-const supabaseAnonKey = 'sb_publishable_lhe3i4VJqJlT6arAq-hVkA_1784t5up'
+// Menggunakan Variable Environment agar lebih aman
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
